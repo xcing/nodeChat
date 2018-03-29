37347c0437fcdef6e46bfbf5d72fe205ae6c918e
xcing <pawanwit.s@gmail.com>
2018-03-29 18:21:27 +0700

    update flow

--- index.js
+++ index.js
@@ -8,24 +8,32 @@ MongoClient.connect("mongodb://localhost:27017", function(err, database) {
 
   	const nodeChatDb = database.db('nodeChat');
     var historyCollection = nodeChatDb.collection('history');
+    var historyData;
 
-	var io = require('socket.io')(http);
-	var port = 3000;
+    historyCollection.find({}).limit(100).toArray(function (e, historyData) {
+    	console.log(historyData);
+    	var io = require('socket.io')(http);
+		var port = 3000;
 
-	app.get('/', function(req, res){
-	  res.sendFile(__dirname + '/index.html');
-	});
+		app.get('/', function(req, res){
+		  res.sendFile(__dirname + '/index.html', {history: historyData});
+		});
 
-	io.on('connection', function(socket){
-	  socket.on('chat message', function(msg){
-	  	historyCollection.insert({"msg": msg});
-	    io.emit('chat message', msg);
-	  });
-	});
+		io.on('connection', function(socket){
+		  socket.on('chat message', function(msg){
+		  	historyCollection.insert({"msg": msg});
+		    io.emit('chat message', msg);
+		  });
+		});
 
-	http.listen(port, function(){
-	  console.log('listening on *:' + port);
-	});
-
+		http.listen(port, function(){
+		  console.log('listening on *:' + port);
+		});
+    });
 });
 
+
+// query last 100
+// show history chat in html page
+// refactor code
+// config for run db to easy
