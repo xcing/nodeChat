e3d260d3e41fc51498b311ea769d28cc9ff37704
Pawanwit <pawanwit.s@gmail.com>
2018-03-30 14:43:40 +0700

    add comment & refactor code

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var MongoClient = require('mongodb').MongoClient;
+const app = require('express')();
+const http = require('http').Server(app);
+const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
 MongoClient.connect("mongodb://localhost:27017", function(err, database) {
   	if(err) { return console.dir(err); }
 
+  	//create database
   	const nodeChatDb = database.db('nodeChat');
-    var historyCollection = nodeChatDb.collection('history');
 
+  	//create collection
+    const historyCollection = nodeChatDb.collection('history');
+
+	//query 100 last message and pass to view
     historyCollection.find({}).sort({_id:-1}).limit(100).toArray(function (e, historyData) {
-    	var io = require('socket.io')(http);
-		var port = 3000;
-
 		app.get('/', function(req, res){
 		  res.render('index', {history: JSON.stringify(historyData.sort(function(a,b) {return (a._id > b._id) ? 1 : ((b._id > a._id) ? -1 : 0);} ))});
 		});
+    });
 
-		io.on('connection', function(socket){
-		  socket.on('chat message', function(msg){
-		  	historyCollection.insert({"msg": msg});
-		    io.emit('chat message', msg);
-		  });
-		});
+    //socket
+    const io = require('socket.io')(http);
+    const port = 3000;
+    io.on('connection', function(socket){
+        socket.on('chat message', function(msg){
+            historyCollection.insert({"msg": msg});
+            io.emit('chat message', msg);
+        });
+    });
 
-		http.listen(port, function(){
-		  console.log('listening on *:' + port);
-		});
+    http.listen(port, function(){
+        console.log('listening on *:' + port);
     });
 });
